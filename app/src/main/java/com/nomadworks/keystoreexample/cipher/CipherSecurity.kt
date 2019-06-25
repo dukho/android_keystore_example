d9a7d69c540ed5dae6e0ae57a9b33164cc56e7bc
dchoi <dchoi@woolworths.com.au>
2019-06-25 15:22:04 +1000

    Refactor CipherSecurity class

--- app/src/main/java/com/nomadworks/keystoreexample/cipher/CipherSecurity.kt
+++ app/src/main/java/com/nomadworks/keystoreexample/cipher/CipherSecurity.kt
@@ -5,7 +5,6 @@ import android.os.Build
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
 import androidx.annotation.RequiresApi
-import com.nomadworks.keystoreexample.BuildConfig
 import java.io.IOException
 import java.security.*
 import java.security.spec.AlgorithmParameterSpec
@@ -15,8 +14,8 @@ import javax.crypto.spec.GCMParameterSpec
 class CipherSecurity {
     companion object {
         val ANDROID_KEY_STORE = "AndroidKeyStore"
-        private val TRANSFORMATION = "AES/GCM/NoPadding"
-        private val FIXED_IV =
+        private const val TRANSFORMATION_ALGORITHM = "AES/GCM/NoPadding"
+        private val FIXED_IV = //initialisation vector
             "0123456789ab".toByteArray() //The IV you use in the encryption must be the same one you use in the decryption
 
     }
@@ -38,9 +37,10 @@ class CipherSecurity {
                 keyGenerator = KeyGenerator
                     .getInstance(KeyProperties.KEY_ALGORITHM_AES, ANDROID_KEY_STORE)
                 keyGenerator.init(
-                    getParameterSpec(alias)
+                    getKeyParameterSpec(alias)
                 )
             } else {
+                //TODO this is not complete - for lower Android version (lower than API 23)
                 keyGenerator = KeyGenerator.getInstance("AES")
                 keyGenerator.init(256, SecureRandom())
             }
@@ -50,7 +50,7 @@ class CipherSecurity {
     }
 
     @RequiresApi(23)
-    private fun getParameterSpec(alias: String): AlgorithmParameterSpec {
+    private fun getKeyParameterSpec(alias: String): AlgorithmParameterSpec {
         return KeyGenParameterSpec.Builder(
             alias,
             KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
@@ -59,6 +59,10 @@ class CipherSecurity {
             .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
             .setRandomizedEncryptionRequired(false)
             .build()
+    }
+
+    private fun getCipherParameterSpec(): AlgorithmParameterSpec {
+        return GCMParameterSpec(128, FIXED_IV) // authentication tag length can be one of 128, 120, 112, 104, 96
     }
 
     @Throws(
@@ -76,9 +80,9 @@ class CipherSecurity {
     )
     internal fun encrypt(textToEncrypt: String, alias: String): ByteArray {
         val key = (keyStore.getEntry(alias, null) as KeyStore.SecretKeyEntry).secretKey
-        val cipher = Cipher.getInstance(TRANSFORMATION)
-        cipher.init(Cipher.ENCRYPT_MODE, key, GCMParameterSpec(128, FIXED_IV))
-        return cipher.doFinal(textToEncrypt.toByteArray())
+        val cipher = Cipher.getInstance(TRANSFORMATION_ALGORITHM)
+        cipher.init(Cipher.ENCRYPT_MODE, key, getCipherParameterSpec())
+        return cipher.doFinal(textToEncrypt.toByteArray(Charsets.UTF_8))
     }
 
     @Throws(
@@ -95,10 +99,9 @@ class CipherSecurity {
     )
     internal fun decrypt(encryptedData: ByteArray, alias: String): String {
         val key = (keyStore.getEntry(alias, null) as KeyStore.SecretKeyEntry).secretKey
-        val cipher = Cipher.getInstance(TRANSFORMATION)
-        val spec = GCMParameterSpec(128, FIXED_IV)
-        cipher.init(Cipher.DECRYPT_MODE, key, spec)
-        return String(cipher.doFinal(encryptedData), charset("UTF-8"))
+        val cipher = Cipher.getInstance(TRANSFORMATION_ALGORITHM)
+        cipher.init(Cipher.DECRYPT_MODE, key, getCipherParameterSpec())
+        return String(cipher.doFinal(encryptedData), Charsets.UTF_8)
     }
 
 }
